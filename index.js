87663d05d8d72c7d8cd7b6659aa03dc446ec3402
odogun maryam aderonke <odogunmaryam@gmail.com>
2023-07-25 11:34:23 +0100

    cake created

---
+++ index.js
@@ -0,0 +1,17 @@
+function setTheme(theme) {
+    let root = document.documentElement;
+    if (theme === 'light') {
+    root.style.setProperty('--bg-color', '#ECF2FF');
+    root.style.setProperty('--text-color', '#2B283A');
+    root.style.setProperty('--title-color', '#4A4E74');
+    } else if (theme === 'dark') {
+    root.style.setProperty('--bg-color', '#2B283A');
+    root.style.setProperty('--text-color', '#ECF2FF');
+    root.style.setProperty('--title-color', '#D5D4D8');
+    }
+    }
+    
+    // 1. Try to change the theme to 'dark'
+    // 2. Run the code to see that it works
+    setTheme('dark');
+    
